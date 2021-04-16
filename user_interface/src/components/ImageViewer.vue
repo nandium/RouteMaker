8244c6b48a7b26dcd2f9a795196a0867f4ff20f5
Yar Khine Phyo <yarkhinephyo@gmail.com>
2021-04-16 14:24:21 +0800

    feat: Add initial user interface (#2)

---
+++ user_interface/src/components/ImageViewer.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <img v-if="isImageUploaded" :src="imageURL" alt="" />
+    <div class="font-weight-normal" v-else>Insert an image!</div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+  name: "ImageViewer",
+  computed: {
+    ...mapGetters("home", {
+      isImageUploaded: "getIsImageUploaded",
+      imageURL: "getImageURL",
+    }),
+  },
+};
+</script>
+
+<style scoped>
+</style>
---
+++ user_interface/src/store/modules/home.js
@@ -0,0 +1,102 @@
+const state = () => ({
+  // items: [],
+  isImageUploaded: false,
+  imageURL: "",
+});
+
+// getters
+const getters = {
+  getIsImageUploaded: (state) => {
+    return state.isImageUploaded;
+  },
+  getImageURL: (state) => {
+    return state.imageURL;
+  },
+
+  // cartProducts: (state, getters, rootState) => {
+  //   return state.items.map(({ id, quantity }) => {
+  //     const product = rootState.products.all.find(product => product.id === id)
+  //     return {
+  //       title: product.title,
+  //       price: product.price,
+  //       quantity
+  //     }
+  //   })
+  // },
+
+  // cartTotalPrice: (state, getters) => {
+  //   return getters.cartProducts.reduce((total, product) => {
+  //     return total + product.price * product.quantity
+  //   }, 0)
+  // }
+};
+
+// mutations
+const mutations = {
+  setIsImageUploaded: (state, isUploaded) => {
+    state.isImageUploaded = isUploaded;
+  },
+  setImageURL: (state, imageURL) => {
+    state.imageURL = imageURL;
+  },
+
+  // pushProductToCart (state, { id }) {
+  //   state.items.push({
+  //     id,
+  //     quantity: 1
+  //   })
+  // },
+
+  // incrementItemQuantity (state, { id }) {
+  //   const cartItem = state.items.find(item => item.id === id)
+  //   cartItem.quantity++
+  // },
+
+  // setCartItems (state, { items }) {
+  //   state.items = items
+  // },
+
+  // setCheckoutStatus (state, status) {
+  //   state.checkoutStatus = status
+  // }
+};
+
+// actions
+const actions = {
+  // checkout ({ commit, state }, products) {
+  //   const savedCartItems = [...state.items]
+  //   commit('setCheckoutStatus', null)
+  //   // empty cart
+  //   commit('setCartItems', { items: [] })
+  //   shop.buyProducts(
+  //     products,
+  //     () => commit('setCheckoutStatus', 'successful'),
+  //     () => {
+  //       commit('setCheckoutStatus', 'failed')
+  //       // rollback to the cart saved before sending the request
+  //       commit('setCartItems', { items: savedCartItems })
+  //     }
+  //   )
+  // },
+  // addProductToCart ({ state, commit }, product) {
+  //   commit('setCheckoutStatus', null)
+  //   if (product.inventory > 0) {
+  //     const cartItem = state.items.find(item => item.id === product.id)
+  //     if (!cartItem) {
+  //       commit('pushProductToCart', { id: product.id })
+  //     } else {
+  //       commit('incrementItemQuantity', cartItem)
+  //     }
+  //     // remove 1 item from stock
+  //     commit('products/decrementProductInventory', { id: product.id }, { root: true })
+  //   }
+  // }
+};
+
+export default {
+  namespaced: true,
+  state,
+  getters,
+  actions,
+  mutations,
+};
