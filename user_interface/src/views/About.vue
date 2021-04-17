ef1a741957f9ac2abc6f89831f17a476c0bb87fd
yarkhinephyo <yarkhinephyo@gmail.com>
2021-04-17 14:15:30 +0800

    feat: Add about page

--- user_interface/src/views/About.vue
+++ user_interface/src/views/About.vue
@@ -1,5 +1,34 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <b-jumbotron>
+    <h1 class="display-4">Allez Climbers!</h1>
+    <p class="lead">
+      Personally, when people make routes for me at the gym, I tend to forget
+      half of the moves with my goldfish memory.
+    </p>
+    <p class="lead">
+      One way to solve this is to label out the routes on mobile, but circling
+      and numbering them takes too much time too.
+    </p>
+    <hr class="my-4" />
+    <p class="lead">1. Upload the photo of a climbing wall</p>
+    <p class="lead">2. Label the holds as handholds or footholds</p>
+    <p class="lead">3. Export it to share with friends</p>
+    <hr class="my-4" />
+    <b-button
+      size="lg"
+      variant="primary"
+      class="mx-2"
+      href="https://github.com/yarkhinephyo/yolo_bouldering"
+      role="button"
+      >Github</b-button
+    >
+    <b-button
+      size="lg"
+      variant="primary"
+      class="mx-2"
+      href="https://www.linkedin.com/in/yar-khine-phyo-93073012b/"
+      role="button"
+      >Contact</b-button
+    >
+  </b-jumbotron>
 </template>
