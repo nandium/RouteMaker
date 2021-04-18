f817be127d8478edc5a0569abeefc681aa9769f4
yarkhinephyo <yarkhinephyo@gmail.com>
2021-04-18 18:26:15 +0800

    docs: Update screenshots by breaking down into more steps

--- user_interface/src/views/About.vue
+++ user_interface/src/views/About.vue
@@ -1,40 +1,51 @@
 <template>
   <b-jumbotron>
-    <h1 class="display-4">Allez Climbers!</h1>
+    <h1 class="display-4">Hello Climbers! &#129495;</h1>
+    <hr class="my-4" />
     <p class="lead">
       Personally, when people make routes for me at the gym, I tend to forget
       half of the moves with my goldfish memory.
     </p>
     <p class="lead">
-      One way to solve this is to label out the routes on mobile, but circling
-      and numbering them takes too much time too.
+      One way is to draw them out on handphone but it is difficult to point out the exact holds with fat fingers.
     </p>
+    <p class="lead">
+      Labeling the exact order of moves can be troublesome too.
+    </p>
+    
     <hr class="my-4" />
     <p class="lead my-4">1. Upload the photo of a climbing wall</p>
-    <img src="@/assets/Screenshot_1.jpg" />
-    <p class="lead my-4">2. Label the holds as handholds or footholds</p>
-    <img src="@/assets/Screenshot_2.jpg" />
-    <p class="lead my-4">3. Export it to share with friends</p>
-    <img src="@/assets/Screenshot_3.jpg" />
+    <img class="border border-secondary" src="@/assets/Screenshot_1.jpg" />
+    <p class="lead my-4">2. The holds will be marked automatically</p>
+    <img class="border border-secondary" src="@/assets/Screenshot_2.jpg" />
+    <p class="lead my-4">3. Select your handholds</p>
+    <img class="border border-secondary" src="@/assets/Screenshot_3.jpg" />
+    <p class="lead my-4">4. Select your footholds</p>
+    <img class="border border-secondary" src="@/assets/Screenshot_4.jpg" />
+    <p class="lead my-4">5. Export as an image to share with friends</p>
+    <img class="border border-secondary" src="@/assets/Screenshot_5.jpg" />
 
     <hr class="my-4" />
+    <p class="lead my-4">Pull requests are welcomed!</p>
+    
     <b-button
       size="lg"
       variant="primary"
       class="mx-2"
       href="https://github.com/yarkhinephyo/yolo_bouldering"
       role="button"
-      >Github</b-button
+      ><b-icon-github /> Github</b-button
     >
     <b-button
       size="lg"
       variant="primary"
       class="mx-2"
-      href="https://www.linkedin.com/in/yar-khine-phyo-93073012b/"
+      href="https://www.linkedin.com/in/yar-khine-phyo/"
       role="button"
-      >Contact</b-button
+      ><b-icon-linkedin /> Linkedin</b-button
     >
   </b-jumbotron>
+  
 </template>
 <style scoped>
 img {
