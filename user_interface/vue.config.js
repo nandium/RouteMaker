a5a46d853b24a704712fadb4514e075057df228f
Yar Khine Phyo <yarkhinephyo@gmail.com>
2021-04-26 11:55:22 +0800

    feat: Add google analytics tracking (#40)

--- user_interface/vue.config.js
+++ user_interface/vue.config.js
@@ -1,8 +1,15 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/RouteMaker/' : '/',
+  publicPath:
+    process.env.NODE_ENV === 'production' && process.env.VUE_APP_PRODUCTION_ROUTE
+      ? process.env.VUE_APP_PRODUCTION_ROUTE
+      : '/',
   chainWebpack: (config) => {
     config.plugin('html').tap((args) => {
       args[0].title = 'Route Maker';
+      args[0].TRACKING_ID =
+        process.env.NODE_ENV === 'production' && process.env.VUE_APP_TRACKING_ID
+          ? process.env.VUE_APP_TRACKING_ID
+          : '';
       return args;
     });
   },
