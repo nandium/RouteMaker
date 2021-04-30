b4939441de0a63bad7112f1843267425badf74de
qwoprocks <46164638+qwoprocks@users.noreply.github.com>
2021-04-30 16:10:13 +0800

    fix: Prettify

--- ionic_user_interface/src/components/Canvas.vue
+++ ionic_user_interface/src/components/Canvas.vue
@@ -1,20 +1,25 @@
 <template>
   <div>
-  <ion-segment class="mode-switcher" @ionChange="modeChanged($event)" value="handhold" color="primary">
-    <ion-segment-button value="handhold">
-      <ion-label>HandHold</ion-label>
-    </ion-segment-button>
-    <ion-segment-button value="foothold">
-      <ion-label>FootHold</ion-label>
-    </ion-segment-button>
-    <ion-segment-button value="drawbox">
-      <ion-label>DrawBox</ion-label>
-    </ion-segment-button>
-    <ion-segment-button value="export">
-      <ion-label>Export</ion-label>
-    </ion-segment-button>
-  </ion-segment>
-  <div id="konva-container"></div>
+    <ion-segment
+      class="mode-switcher"
+      @ionChange="modeChanged($event)"
+      value="handhold"
+      color="primary"
+    >
+      <ion-segment-button value="handhold">
+        <ion-label>HandHold</ion-label>
+      </ion-segment-button>
+      <ion-segment-button value="foothold">
+        <ion-label>FootHold</ion-label>
+      </ion-segment-button>
+      <ion-segment-button value="drawbox">
+        <ion-label>DrawBox</ion-label>
+      </ion-segment-button>
+      <ion-segment-button value="export">
+        <ion-label>Export</ion-label>
+      </ion-segment-button>
+    </ion-segment>
+    <div id="konva-container"></div>
   </div>
 </template>
 
@@ -28,67 +33,67 @@ Konva.pixelRatio = 1;
 export default defineComponent({
   name: 'Canvas',
   components: {
-      IonLabel,
-      IonSegment,
-      IonSegmentButton,
+    IonLabel,
+    IonSegment,
+    IonSegmentButton,
   },
   props: {
-      imgSrc: {
-          type: String,
-          required: true
-      },
-      width: {
-          type: Number,
-          required: true
-      },
+    imgSrc: {
+      type: String,
+      required: true,
+    },
+    width: {
+      type: Number,
+      required: true,
+    },
   },
   setup(props) {
     const stage = ref<Konva.Stage | null>(null);
     const layer = ref<Konva.Layer | null>(null);
 
     const updateImage = () => {
-        const image = new Image();
-        image.onload = () => {
-            if (stage.value !== null && layer.value !== null) {
-                stage.value?.width(props.width);
-                stage.value?.height(props.width / image.width * image.height);
-                const konvaImage = new Konva.Image({
-                    x: 0,
-                    y: 0,
-                    image: image,
-                    width: props.width,
-                    height: props.width / image.width * image.height
-                });
+      const image = new Image();
+      image.onload = () => {
+        if (stage.value !== null && layer.value !== null) {
+          stage.value?.width(props.width);
+          stage.value?.height((props.width / image.width) * image.height);
+          const konvaImage = new Konva.Image({
+            x: 0,
+            y: 0,
+            image: image,
+            width: props.width,
+            height: (props.width / image.width) * image.height,
+          });
 
-                layer.value.add(konvaImage);
-                layer.value.batchDraw();
-            }
-        };
-        image.src = props.imgSrc;
+          layer.value.add(konvaImage);
+          layer.value.batchDraw();
+        }
+      };
+      image.src = props.imgSrc;
     };
 
     onMounted(() => {
-        stage.value = new Konva.Stage({
-            container: 'konva-container',
-        });
-        const tempLayer = new Konva.Layer();
-        layer.value = tempLayer;
-        stage.value.add(tempLayer);
-        updateImage();
+      stage.value = new Konva.Stage({
+        container: 'konva-container',
+      });
+      const tempLayer = new Konva.Layer();
+      layer.value = tempLayer;
+      stage.value.add(tempLayer);
+      updateImage();
     });
     watch(() => props.imgSrc, updateImage);
     watch(() => props.width, updateImage);
 
     return {
-        stage,
-        layer,
-    }
+      stage,
+      layer,
+    };
   },
   methods: {
     modeChanged(e: CustomEvent) {
       console.log(e.detail.value);
-    }
-  }
+    },
+  },
 });
 </script>
 
