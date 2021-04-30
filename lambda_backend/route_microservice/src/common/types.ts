0e4b0eb3b371b55d21c52eaca33addb7ca25b010
yarkhinephyo <yarkhinephyo@gmail.com>
2021-04-30 21:47:20 +0800

    feat: Add getAllGyms endpoint in Route microservice

---
+++ lambda_backend/route_microservice/src/common/types.ts
@@ -0,0 +1,5 @@
+export interface GetAllGymsEvent {
+  queryStringParameters: {
+    country: string;
+  };
+}
---
+++ lambda_backend/route_microservice/src/getAllGyms.ts
@@ -0,0 +1,51 @@
+import { Handler } from 'aws-lambda';
+import DynamoDB, { AttributeValue, ScanInput, QueryInput } from 'aws-sdk/clients/dynamodb';
+import { getMiddlewareAddedHandler, GetAllGymsEvent } from './common';
+import createError from 'http-errors';
+
+const dynamoDb = new DynamoDB.DocumentClient();
+
+const getAllGyms: Handler = async (event: GetAllGymsEvent) => {
+  if (!process.env['GYM_TABLE_NAME']) {
+    throw createError(400, 'Gym table name is not set');
+  }
+  const {
+    queryStringParameters: { country },
+  } = event;
+  // QueryString of country exists, query by country. If not, return all gyms.
+  if (country) {
+    const queryInput: QueryInput = {
+      TableName: process.env['GYM_TABLE_NAME'],
+      ConsistentRead: false,
+      KeyConditionExpression: 'country = :country',
+      ExpressionAttributeValues: {
+        ':country': country as AttributeValue,
+      },
+    };
+    try {
+      const { Items } = await dynamoDb.query(queryInput).promise();
+      return {
+        statusCode: 200,
+        body: JSON.stringify({ Message: 'Query all gyms success', Items }),
+      };
+    } catch (error) {
+      throw createError(400, 'Error querying table :' + error.stack);
+    }
+  } else {
+    const scanInput: ScanInput = {
+      TableName: process.env['GYM_TABLE_NAME'],
+      ConsistentRead: false,
+    };
+    try {
+      const { Items } = await dynamoDb.scan(scanInput).promise();
+      return {
+        statusCode: 200,
+        body: JSON.stringify({ Message: 'Scan all gyms success', Items }),
+      };
+    } catch (error) {
+      throw createError(400, 'Error scanning table :' + error.stack);
+    }
+  }
+};
+
+export const handler = getMiddlewareAddedHandler(getAllGyms);
