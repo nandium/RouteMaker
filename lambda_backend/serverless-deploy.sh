c23908a5fb8c1ae207f3e2c5f8c78d3c8b8b71f2
yarkhinephyo <yarkhinephyo@gmail.com>
2021-05-30 19:45:06 +0800

    refactor: Change database setup dir folder name

--- lambda_backend/serverless-deploy.sh
+++ lambda_backend/serverless-deploy.sh
@@ -11,7 +11,7 @@ if [ $1 == "deploy" ]
 then
   cd ./cognito_setup
   sls deploy --stage $2
-  cd ../route_database_setup
+  cd ../database_setup
   sls deploy --stage $2
   cd ../s3_bucket_setup
   sls deploy --stage $2
@@ -36,7 +36,7 @@ then
   sls remove --stage $2
   cd ../s3_bucket_setup
   sls remove --stage $2
-  cd ../route_database_setup
+  cd ../database_setup
   sls remove --stage $2
   cd ../cognito_setup
   sls remove --stage $2
