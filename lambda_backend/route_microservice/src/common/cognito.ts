4ab65be79a4e96d5eb712dc572c4a8f3ea8fae39
Yar Khine Phyo <yarkhinephyo@gmail.com>
2021-06-07 00:29:00 +0800

    Feat/return route owner username (#75)
    * feat: Add owner'displayName when returning route details
    
    * docs: Update API docs

--- lambda_backend/route_microservice/src/common/cognito.ts
+++ lambda_backend/route_microservice/src/common/cognito.ts
@@ -1,4 +1,6 @@
 import CognitoIdentityServiceProvider, {
+  AdminGetUserRequest,
+  AttributeType,
   GetUserRequest,
 } from 'aws-sdk/clients/cognitoidentityserviceprovider';
 import createError from 'http-errors';
@@ -11,12 +13,30 @@ export const getCognitoUserDetails = async (AccessToken: string): Promise<Cognit
   const getUserParams: GetUserRequest = { AccessToken };
   try {
     const { UserAttributes } = await cognitoIdentity.getUser(getUserParams).promise();
-    const userEmail = UserAttributes.filter((attribute) => attribute.Name === 'email')[0]
-      .Value as string;
-    const fullName = UserAttributes.filter((attribute) => attribute.Name === 'name')[0]
-      .Value as string;
-    return { fullName, userEmail };
+    return parseUserAttributes(UserAttributes);
   } catch (error) {
     throw createError(500, 'Error retrieving Cognito user details');
   }
+};
+
+export const adminGetCognitoUserDetails = async (Username: string): Promise<CognitoUserDetails> => {
+  const adminGetUserParams: AdminGetUserRequest = {
+    Username,
+    UserPoolId: process.env['COGNITO_USERPOOL_ID'] as string,
+  };
+  try {
+    const adminGetUserResponse = await cognitoIdentity.adminGetUser(adminGetUserParams).promise();
+    const UserAttributes = adminGetUserResponse.UserAttributes as AttributeType[];
+    return parseUserAttributes(UserAttributes);
+  } catch (error) {
+    throw createError(500, 'Error retrieving Cognito user details');
+  }
+};
+
+const parseUserAttributes = (UserAttributes: AttributeType[]): CognitoUserDetails => {
+  const userEmail = UserAttributes.filter((attribute) => attribute.Name === 'email')[0]
+    .Value as string;
+  const fullName = UserAttributes.filter((attribute) => attribute.Name === 'name')[0]
+    .Value as string;
+  return { fullName, userEmail };
 };
