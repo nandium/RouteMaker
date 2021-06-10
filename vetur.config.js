b7712db21db0cde85ae1c67dad074efac36e8268
qwoprocks <46164638+qwoprocks@users.noreply.github.com>
2021-06-10 10:15:37 +0800

    fix: Resolve linting issues

---
+++ vetur.config.js
@@ -0,0 +1,8 @@
+// vetur.config.js
+/** @type {import('vls').VeturConfig} */
+module.exports = {
+  settings: {
+    'vetur.validation.template': false,
+  },
+  projects: ['./ionic_user_interface'],
+};
