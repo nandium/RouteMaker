8ab4f7f0739e25b8440a0f450f43d850ea4134cf
yarkhinephyo <yarkhinephyo@gmail.com>
2021-06-12 14:13:10 +0800

    feat: Update backend validation for ascii

--- lambda_backend/route_microservice/src/common/schema.ts
+++ lambda_backend/route_microservice/src/common/schema.ts
@@ -2,7 +2,7 @@ const ISODateStringPattern =
   '^\\d{4}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])T\\d{2}:\\d{2}:\\d{2}.\\d{3}Z$';
 const AlphanumericSpaceHyphen = '^[a-zA-Z0-9 \\-]*$';
 const NumericDecimalCommaSpace = '^[0-9., ]*$';
-const AsciiCharacters = '^[\\x00-\\x7F]+$';
+const AsciiCharacters = '^[ -~]+$';
 
 export const createRouteSchema = {
   type: 'object',
