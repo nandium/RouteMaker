f8b06ed7e7fdda5a11fadd1122a07232778e6ba8
qwoprocks <46164638+qwoprocks@users.noreply.github.com>
2021-06-13 20:55:32 +0800

    fix: Reduce console logging upon axios error

--- ionic_user_interface/src/providers.ts
+++ ionic_user_interface/src/providers.ts
@@ -27,8 +27,6 @@ const forceLogout = async (): Promise<void> => {
         // The request was made and the server responded with a status code
         // that falls out of the range of 2xx
         console.log(error.response.data);
-        console.log(error.response.status);
-        console.log(error.response.headers);
       } else if (error.request) {
         // The request was made but no response was received
         // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
