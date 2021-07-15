781962a98e44015bf42fc6b27bc048b7f51767b4
yarkhinephyo <yarkhinephyo@gmail.com>
2021-07-15 15:04:20 +0800

    feat: Add UX flow to routemaking process

--- ionic_user_interface/src/components/wall-image-viewer/Canvas.vue
+++ ionic_user_interface/src/components/wall-image-viewer/Canvas.vue
@@ -2,72 +2,29 @@
   <div>
     <br />
     <br />
-    <div class="button-rows">
-      <ion-item>
+    <div class="sticky-button-rows">
+      <ion-item class="instruction-steps">
         <ion-icon
+          v-if="routeMakingStep > 0"
           slot="start"
           :icon="chevronBackOutline"
-          @click="routeMakingStepNumber--"
+          @click="changeRouteMakingStep(false)"
         ></ion-icon>
-        <ion-text v-if="routeMakingStepNumber === 0">Drag to draw unmarked holds</ion-text>
-        <ion-text v-if="routeMakingStepNumber === 1">Select handholds &#38; footholds</ion-text>
+        <ion-text v-if="routeMakingStep === 0">Draw unmarked rocks. Pinch to zoom</ion-text>
+        <ion-text v-if="routeMakingStep === 1">Select handholds &#38; footholds</ion-text>
+        <ion-text v-if="routeMakingStep === 2">Do handhold orders matter?</ion-text>
+        <ion-text v-if="routeMakingStep === 3">One hand or two hand start?</ion-text>
+        <ion-text v-if="routeMakingStep === 4">Export locally or post on wall?</ion-text>
         <ion-icon
+          v-if="routeMakingStep < 4"
           slot="end"
           :icon="chevronForwardOutline"
-          @click="routeMakingStepNumber++"
+          @click="changeRouteMakingStep(true)"
         ></ion-icon>
       </ion-item>
-      <ion-segment
-        class="mode-switcher"
-        @ionChange="selectModeChanged($event)"
-        color="tertiary"
-        mode="ios"
-        :value="selectedMode"
-        v-if="false"
-      >
-        <ion-segment-button :value="SelectMode.HANDHOLD">
-          <ion-label>HandHold</ion-label>
-        </ion-segment-button>
-        <ion-segment-button :value="SelectMode.FOOTHOLD">
-          <ion-label>FootHold</ion-label>
-        </ion-segment-button>
-      </ion-segment>
-      <div v-if="+selectedMode === SelectMode.MARKDONE">
-        <ion-button
-          @click="handleExportClick"
-          class="solid-button"
-          fill="outline"
-          color="secondary"
-        >
-          Export
-        </ion-button>
-        <ion-button @click="handlePostClick" class="solid-button" fill="solid" color="primary">
-          Post
-        </ion-button>
-      </div>
-      <div v-if="+selectedMode === SelectMode.HANDHOLD || +selectedMode === SelectMode.FOOTHOLD">
-        <ion-button
-          class="outline-button"
-          @click="handleHideNumbersClick"
-          :fill="hideNumbersFill"
-          color="tertiary"
-        >
-          {{ hideNumbersText }}
-        </ion-button>
-        <ion-button
-          class="outline-button"
-          @click="handleTapeClick"
-          :fill="tapeFill"
-          color="primary"
-        >
-          {{ tapeText }}
-        </ion-button>
-        <ion-button class="solid-button" fill="solid" color="danger" @click="handleReset">
-          Reset
-        </ion-button>
-      </div>
+
       <ion-button
-        v-if="+selectedMode === SelectMode.DRAWBOX"
+        v-if="routeMakingStep === 0"
         @click="handleUndoDraw"
         class="solid-button"
         fill="solid"
@@ -75,6 +32,46 @@
       >
         Undo Draw
       </ion-button>
+      <ion-row class="hold-picker" v-if="routeMakingStep === 1">
+        <ion-col size-xs="9">
+          <ion-segment
+            @ionChange="changeSelectMode($event.detail.value)"
+            color="tertiary"
+            mode="ios"
+            :value="selectedMode"
+          >
+            <ion-segment-button :value="SelectMode.HANDHOLD">
+              <ion-label>HandHold</ion-label>
+            </ion-segment-button>
+            <ion-segment-button :value="SelectMode.FOOTHOLD">
+              <ion-label>FootHold</ion-label>
+            </ion-segment-button>
+          </ion-segment>
+        </ion-col>
+        <ion-col size-xs="3">
+          <ion-button class="solid-button" fill="solid" color="danger" @click="handleReset">
+            Reset
+          </ion-button>
+        </ion-col>
+      </ion-row>
+      <div v-if="routeMakingStep === 2">
+        <ion-button class="outline-button" @click="handleHideNumbersClick" color="tertiary">
+          {{ hideNumbersText }}
+        </ion-button>
+      </div>
+      <div v-if="routeMakingStep === 3">
+        <ion-button class="outline-button" @click="handleTapeClick" color="primary">
+          {{ tapeText }}
+        </ion-button>
+      </div>
+      <div v-if="routeMakingStep === 4">
+        <ion-button @click="handleExportClick" class="solid-button" color="medium">
+          Export
+        </ion-button>
+        <ion-button @click="handlePostClick" class="solid-button" fill="solid" color="primary">
+          Post
+        </ion-button>
+      </div>
     </div>
     <div id="konva-container" class="konva-container"></div>
   </div>
@@ -82,7 +79,18 @@
 
 <script lang="ts">
 import Konva from 'konva';
-import { IonButton, IonLabel, IonSegment, IonSegmentButton, IonContent, IonIcon } from '@ionic/vue';
+import {
+  IonButton,
+  IonLabel,
+  IonSegment,
+  IonSegmentButton,
+  IonContent,
+  IonIcon,
+  IonItem,
+  IonText,
+  IonRow,
+  IonCol,
+} from '@ionic/vue';
 import { defineComponent, inject, onMounted, ref, watch, PropType } from 'vue';
 import { useRouter } from 'vue-router';
 import { chevronForwardOutline, chevronBackOutline } from 'ionicons/icons';
@@ -90,12 +98,7 @@ import { chevronForwardOutline, chevronBackOutline } from 'ionicons/icons';
 import getBoundingBoxes from '@/components/wall-image-viewer/getBoundingBoxes';
 import { useBoxLayer, DrawLayer } from '@/components/wall-image-viewer/box-layer';
 import { downloadURI } from '@/common/download';
-import {
-  SelectMode,
-  TapeMode,
-  NumberMode,
-  ModeChangedEvent,
-} from '@/components/wall-image-viewer/types';
+import { SelectMode, TapeMode, NumberMode } from '@/components/wall-image-viewer/types';
 import {
   addKonvaListenerPinchZoom,
   addKonvaListenerTouchMove,
@@ -112,6 +115,10 @@ export default defineComponent({
     IonSegment,
     IonSegmentButton,
     IonIcon,
+    IonItem,
+    IonText,
+    IonRow,
+    IonCol,
   },
   props: {
     imgSrc: {
@@ -132,12 +139,10 @@ export default defineComponent({
     const selectedMode = ref<SelectMode>(SelectMode.DRAWBOX);
     const tapeMode = ref<TapeMode>(TapeMode.NONE);
     const numberMode = ref<NumberMode>(NumberMode.ON);
-    const hideNumbersFill = ref<string>('outline');
     const hideNumbersText = ref<string>('Hide Numbers');
-    const tapeFill = ref<string>('outline');
     const tapeText = ref<string>('No Tape');
     const loadingSpinner = inject('$loading') as Record<string, any>;
-    const routeMakingStepNumber = ref(0);
+    const routeMakingStep = ref(0);
 
     const setRouteImageUri: (imageUri: string) => void = inject(
       'setRouteImageUri',
@@ -187,10 +192,43 @@ export default defineComponent({
         imageLayer.batchDraw();
 
         // Listeners must be added after image is loaded
+        // Currently initial listeners are for SelectMode.DRAWBOX
         addKonvaListenerPinchZoom(stage);
-        addKonvaListenerTouchMove(stage, props.ionContent);
+        DrawLayer.addKonvaDrawLayer(stage);
       };
       image.src = props.imgSrc;
+    };
+
+    onMounted(async () => {
+      stage = new Konva.Stage({
+        container: 'konva-container',
+      });
+      imageLayer.listening(false);
+      imageLayer.add(konvaImage);
+      stage.add(imageLayer);
+      stage.add(boxLayer);
+
+      await loadImageOnStage();
+    });
+
+    /**
+     * Changing to drawing mode -> Draw layer is added
+     * Changing away from drawing mode -> Boxes are exported from Draw layer into BoxLayer
+     */
+    const changeSelectMode = (newSelectedMode: SelectMode) => {
+      const oldSelectedMode = selectedMode.value;
+      if (+newSelectedMode === SelectMode.DRAWBOX) {
+        offKonvaStageListeners(stage);
+        addKonvaListenerPinchZoom(stage);
+        DrawLayer.addKonvaDrawLayer(stage);
+      } else if (+oldSelectedMode === SelectMode.DRAWBOX) {
+        const newBoundingBoxes = DrawLayer.getKonvaDrawLayerBoundingBoxes(stage);
+        DrawLayer.removeKonvaDrawLayer(stage);
+        addKonvaListenerTouchMove(stage, props.ionContent);
+        addBoxLayerBoundingBoxes(newBoundingBoxes);
+        imageLayer.batchDraw();
+      }
+      selectedMode.value = newSelectedMode;
     };
 
     const resizeStage = () => {
@@ -206,35 +244,13 @@ export default defineComponent({
         DrawLayer.resizeDrawLayer(stage, factor);
       }
     };
-
-    /**
-     * Changing to drawing mode -> Draw layer is added
-     * Changing away from drawing mode -> Boxes are exported from Draw layer into BoxLayer
-     */
-    const selectModeChanged = (event: ModeChangedEvent) => {
-      const oldSelectedMode = selectedMode.value;
-      const newSelectedMode = event.detail.value;
-      if (+newSelectedMode === SelectMode.DRAWBOX) {
-        offKonvaStageListeners(stage);
-        addKonvaListenerPinchZoom(stage);
-        DrawLayer.addKonvaDrawLayer(stage);
-      } else if (+oldSelectedMode === SelectMode.DRAWBOX) {
-        const newBoundingBoxes = DrawLayer.getKonvaDrawLayerBoundingBoxes(stage);
-        DrawLayer.removeKonvaDrawLayer(stage);
-        addKonvaListenerTouchMove(stage, props.ionContent);
-        addBoxLayerBoundingBoxes(newBoundingBoxes);
-        imageLayer.batchDraw();
-      }
-      selectedMode.value = newSelectedMode;
-    };
+    watch(() => props.width, resizeStage);
 
     const handleHideNumbersClick = () => {
       if (numberMode.value === NumberMode.ON) {
-        hideNumbersFill.value = 'solid';
         hideNumbersText.value = 'Unhide Numbers';
         numberMode.value = NumberMode.OFF;
       } else {
-        hideNumbersFill.value = 'outline';
         hideNumbersText.value = 'Hide Numbers';
         numberMode.value = NumberMode.ON;
       }
@@ -258,14 +274,12 @@ export default defineComponent({
 
     const handleTapeClick = () => {
       if (tapeText.value === 'No Tape') {
-        tapeFill.value = 'solid';
         tapeText.value = '1-Hold Start';
         tapeMode.value = TapeMode.SINGLE_START;
       } else if (tapeText.value === '1-Hold Start') {
         tapeText.value = '2-Hold Start';
         tapeMode.value = TapeMode.DUAL_START;
       } else {
-        tapeFill.value = 'outline';
         tapeText.value = 'No Tape';
         tapeMode.value = TapeMode.NONE;
       }
@@ -277,18 +291,6 @@ export default defineComponent({
       }
     };
 
-    onMounted(async () => {
-      stage = new Konva.Stage({
-        container: 'konva-container',
-      });
-      imageLayer.listening(false);
-      imageLayer.add(konvaImage);
-      stage.add(imageLayer);
-      stage.add(boxLayer);
-
-      await loadImageOnStage();
-    });
-
     watch(
       () => props.imgSrc,
       async () => {
@@ -296,49 +298,81 @@ export default defineComponent({
         await loadImageOnStage();
       },
     );
-    watch(() => props.width, resizeStage);
+
+    /**
+     * Depending on the step of the route making process, the canvas mode is switched accordingly
+     */
+    const changeRouteMakingStep = (doIncrement: boolean) => {
+      if (doIncrement) {
+        if (routeMakingStep.value + 1 > 4) return;
+        routeMakingStep.value = routeMakingStep.value + 1;
+      } else {
+        if (routeMakingStep.value - 1 < 0) return;
+        routeMakingStep.value = routeMakingStep.value - 1;
+      }
+      switch (routeMakingStep.value) {
+        case 0:
+          changeSelectMode(SelectMode.DRAWBOX);
+          break;
+        case 1:
+          changeSelectMode(SelectMode.HANDHOLD);
+          break;
+        case 2:
+          changeSelectMode(SelectMode.MARKDONE);
+          break;
+        case 3:
+          changeSelectMode(SelectMode.MARKDONE);
+          break;
+        case 4:
+          changeSelectMode(SelectMode.MARKDONE);
+          break;
+      }
+    };
 
     return {
       SelectMode,
-      selectModeChanged,
       selectedMode,
-      hideNumbersFill,
       hideNumbersText,
       handleHideNumbersClick,
-      tapeFill,
       tapeText,
       handleTapeClick,
       handleExportClick,
       handlePostClick,
       handleUndoDraw,
       handleReset,
-      routeMakingStepNumber,
+      routeMakingStep,
       chevronForwardOutline,
       chevronBackOutline,
+      changeRouteMakingStep,
+      changeSelectMode,
     };
   },
 });
 </script>
 
 <style scoped>
-ion-item {
+.instruction-steps {
   max-width: 500px;
   margin: 0 auto;
   border-radius: 5px;
   border: 2px solid var(--ion-color-tertiary);
   filter: hue-rotate(90deg);
-  background: transparent;
+  background-color: var(--ion-background-color);
+}
+
+.hold-picker {
+  max-width: 500px;
+  margin: 0 auto;
 }
 
 ion-segment {
-  max-width: 500px;
-  margin: 0 auto;
+  margin: 2px auto;
   height: 44px;
   border-radius: 3px;
   border: 2px solid var(--ion-color-tertiary);
   padding: 1px 3px;
   filter: hue-rotate(90deg);
-  background: transparent;
+  background-color: var(--ion-background-color);
 }
 
 ion-segment-button {
@@ -382,8 +416,7 @@ ion-button::part(native) {
   margin: 0 auto;
 }
 
-.button-rows {
-  /* background-color: var(--ion-background-color); */
+.sticky-button-rows {
   position: sticky;
   position: -webkit-sticky; /* Safari */
   top: 0px;
